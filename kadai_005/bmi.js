67645ef7e6ce1380ae78eceeecb569001e28b25c
たかひろ <55275865+taka-photo@users.noreply.github.com>
2025-08-15 23:45:26 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,16 @@
+//変数の宣言
+let weight;
+let height;
+
+//値の代入
+weight = 68;
+height = 1.7;
+
+//コンソールへの出力
+console.log(weight / (height * height));
+
+
+const bmi = weight / (height * height);
+
+//コンソールへの出力
+console.log(bmi);
