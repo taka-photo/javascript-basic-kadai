afbe7a8c341aaf2e620dab79a628795d55aa3dc9
たかひろ <55275865+taka-photo@users.noreply.github.com>
2025-10-19 07:58:46 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,17 @@
+
+const addBtn = document.getElementById('btn');
+
+const parentList = document.getElementById('text');
+
+
+addBtn.addEventListener('click', () => {
+    
+  const childList = document.createElement('text');
+  childList.textContent = 'ボタンをクリックしました';
+
+  while (parentList.firstChild) {
+    parentList.removeChild(parentList.firstChild);
+  }
+
+  parentList.appendChild(childList);
+});
