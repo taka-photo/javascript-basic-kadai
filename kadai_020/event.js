2b6127a6adf5b1d1cf14274065c54e6e8782ed00
たかひろ <55275865+taka-photo@users.noreply.github.com>
2025-10-23 20:18:45 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,17 +1,7 @@
 
 const addBtn = document.getElementById('btn');
-
 const parentList = document.getElementById('text');
 
-
 addBtn.addEventListener('click', () => {
-    
-  const childList = document.createElement('text');
-  childList.textContent = 'ボタンをクリックしました';
-
-  while (parentList.firstChild) {
-    parentList.removeChild(parentList.firstChild);
-  }
-
-  parentList.appendChild(childList);
+  parentList.textContent = 'ボタンをクリックしました';
 });
